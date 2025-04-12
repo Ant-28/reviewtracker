8ed3276361c628efa626547f7bf2390bdfe7e5ea
Ant <ananthajit5d@gmail.com>
2025-04-12 12:18:48 -0700

    ESLint support for frontend

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,5 +6,6 @@ import globals from "globals";
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { files: ["**/frontend/*.js"], languageOptions : {browser : true}},
   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.node } },
 ]);

78d635f4701ac8012ccffbb6c77623ee0b43ad42
Neil Grover <neil@zyra-ai.com>
2025-04-12 22:21:14 -0700

    js click testing

--- Scraper/facebook_reviews.py
+++ Scraper/facebook_reviews.py
@@ -34,17 +34,14 @@ MAX_ELEMS = 10
 def get_facebook_site(name : str, driver: uc.Chrome) -> Optional[str]:
     # get name of place, return optional string
     driver.get("https://www.google.com")
-    wait = WebDriverWait(driver, 30)
-    search_box = wait.until(EC.element_to_be_clickable((By.ID, "APjFqb")))
-    search_box.click()
-    search_box.click()
-    # driver.execute("arguments[0].click();", search_box)
-    # driver.execute("arguments[0].click();", search_box)
+    wait = WebDriverWait(driver, 15)
+    search_box = wait.until(EC.presence_of_element_located((By.ID, "APjFqb")))
+    driver.execute_script("arguments[0].click();", search_box)
     search_box.send_keys(f"{name} site:facebook.com")
     search_box.send_keys(u'\ue007')
     #        //*[@id="rso"]/div[1]/div/div/div[1]/div/div[2]/div/div/span/a
     xpath = "//*[@id='rso']/div[1]/div/div/div[1]/div/div[2]/div/div/span/a"
- 
+
     site_name = None
     try:
         reviews_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, xpath)))
@@ -59,15 +56,15 @@ def get_facebook_site(name : str, driver: uc.Chrome) -> Optional[str]:
     wait = WebDriverWait(driver, 15)    
 
     # Wait for the search box to be present and input the address
-    close_box = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@aria-label="Close"]')))
-    close_box.click()
+    close_box = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@aria-label="Close"]')))
+    driver.execute_script("arguments[0].click();", close_box)
     review_score_xpath = '/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[4]/div[2]/div/div[1]/div[2]/div/div[1]/div/div/div/div/div[2]/div[2]/div/ul/div[9]/div[2]/a/div/div/span'
     review_score_box = wait.until(EC.presence_of_element_located((By.XPATH, review_score_xpath)))
     overall_score_fb = float(re.search(r"(\d+)\%.*", review_score_box.text).group(1))/100 * 5
 
     driver.get(f"{site_name}reviews")
     close_box = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@aria-label="Close"]')))
-    close_box.click()    
+    driver.execute_script("arguments[0].click();", close_box)  
     
     # I hate this website
     driver.execute_script("a = document.querySelector('html'); a.scrollTop = 0.45*a.scrollHeight; a.scrollTop = 0.45*a.scrollHeight;")
@@ -96,19 +93,18 @@ def get_facebook_site(name : str, driver: uc.Chrome) -> Optional[str]:
 
 def main(argv):
     # Set up headless browser options
-    # options = webdriver.EdgeOptions()
-    # options.page_load_strategy = "eager"
+    options = webdriver.ChromeOptions()
+    options.page_load_strategy = "eager"
     # options.add_experimental_option(
     # "prefs", {"profile.managed_default_content_settings.images": 2}
     # )
-    # options.add_argument("--headless")
-    
+    options.add_argument("--headless")
     # preferences = {
     # "profile.managed_default_content_settings.images": 2,
     # "profile.default_content_settings.images": 2
     # }
     
-    # options.add_argument(f'--disk-cache-dir={os.path.dirname(os.path.realpath(__file__))}')
+    options.add_argument(f'--disk-cache-dir={os.path.dirname(os.path.realpath(__file__))}')
     # remove devtools listening
     # options.add_experimental_option('excludeSwitches', ['enable-logging'])
     # options.add_experimental_option("prefs", preferences)
@@ -116,9 +112,7 @@ def main(argv):
         headless=False,
         use_subprocess=False,
         )
-    
-    
-    # driver = webdriver.ChromiumEdge(options=options)
+    # driver = webdriver.Chrome(options=options)
     # print(f"\033[31;1;{argv[1]}\033[0m", file=stderr)
    
     get_facebook_site(argv[1], driver)
@@ -132,12 +126,10 @@ def main(argv):
 if __name__ == "__main__":
     try:
         main(argv)
-    except Exception as e:
+    except:
         print(json.dumps({
             "reviews" : [],
             "text_avg_rating" : 0,
             "overall_avg_rating" : 0,
             "sentiments" : {},
         }))
-        print(e)
-
